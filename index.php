be9ae2b484374e7dddcb7b1fe5c04bfb3e86f15b
Андрей <fidler.andrey@gmail.com>
2020-08-23 22:07:49 +0300

    Форматирование цены

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ $user_name = ''; // укажите здесь ваше имя
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=format_sum($value['price']) ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -125,10 +125,18 @@ $user_name = ''; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
-        <?php endforeach;?>
-            
-         
-            
+        <?php endforeach;?> 
+
+        <?php function format_sum ($input) {
+            $cel = ceil($input);
+            if ($cel >= 1000) {
+                $cel = number_format($cel, 0, ',', ' ');
+            } 
+            $cel = $cel . " ₽";
+             return $cel;
+         }
+
+         ?>                                
         </ul>
     </section>
 </main>
